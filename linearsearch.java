008919df56376274d6a75203fcb98e2ecbc5c82b
sauravkumar0007 <shivamsaurav7479@gmail.com>
2026-07-17 19:11:28 +0530

    Create linearsearch.java

---
+++ linearsearch.java
@@ -0,0 +1,24 @@
+public class linearsearch{
+    public static  void main (String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int target = 5;
+        LinearSearch(arr, target);
+    }
+ 
+public static void LinearSearch(int arr[], int target){
+    int ans = -1;
+    for (int i = 0; i<arr.length; i++){
+        if(arr[i] == target){
+            ans = i;
+            break;
+        }
+    }
+    if(ans == -1){
+        System.out.println("Element not found");
+    }
+    else{
+        System.out.println("Element found at index: " + ans);
+    }
+}
+
+}
