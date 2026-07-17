2b43641a7e3aa07264c746ae331d344bb3cc14d9
sauravkumar0007 <shivamsaurav7479@gmail.com>
2026-07-17 19:27:09 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -22,3 +22,34 @@ public static void LinearSearch(int arr[], int target){
 }
 
 }
+
+// search multiple elements
+public class linearsearch{
+    public static  void main (String[] args){
+        int arr[] = {1,2,5,4,5,6,5,8,9};
+        int target = 5;
+        LinearSearchMultiple(arr, target);
+    }
+ 
+public static void LinearSearchMultiple(int arr[], int target){
+  
+   int ans[] = new int[arr.length];
+   int k = 0;
+    for (int i = 0; i<arr.length; i++){
+        if(arr[i] == target){
+      
+            ans[k] = i;
+            k++;
+           
+        }
+    }
+    if(k == 0){
+        System.out.println("Element not found");
+    }
+    else{
+        for(int i=0; i<k;i++)
+            System.out.println("Element found at index: " + ans[i]);
+    }
+}
+
+}
